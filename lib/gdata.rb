3cdb274b7fa935091cbc70def88930666ea18986
GordonDiggs <gordon@gordondiggs.com>
2010-08-02 10:36:02 -0400

    Require jcode only if using older than 1.9 - could probably include the kcode line in there, too...

--- lib/gdata.rb
+++ lib/gdata.rb
@@ -18,5 +18,7 @@ require 'gdata/http'
 require 'gdata/client'
 require 'gdata/auth'
 # This is for Unicode "support"
-require 'jcode'
+if RUBY_VERSION < '1.9'
+  require 'jcode'
+end
 $KCODE = 'UTF8'
